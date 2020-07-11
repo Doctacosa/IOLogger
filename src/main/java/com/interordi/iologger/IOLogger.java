db88f373889b6e1f223ece81cb27f8b8952158fb
Stéphane Lemay <interordi@interordi.com>
2020-07-11 11:30:42 -0400

    Remove test code

--- src/main/java/com/interordi/iologger/IOLogger.java
+++ src/main/java/com/interordi/iologger/IOLogger.java
@@ -6,13 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class IOLogger extends JavaPlugin {
 
 	public void onEnable() {
-		new LoginListener(this);
 		new ChatListener(this);
-		/*
-		MyCommandExecutor executor = new MyCommandExecutor(this);
-		getCommand("basic").setExecutor(executor);
-		getCommand("basic2").setExecutor(executor);
-		*/
 		
 		getLogger().info("IOLogger enabled");
 	}
